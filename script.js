9aa556de82c21fd41f8cd3052b9fd83537166d7f
gamalabdu <88411543+gamalabdu@users.noreply.github.com>
2022-01-06 22:41:07 -0500

    Home Page Buttons Fixed
    Fixed the homepage button by switching the input tag to "type = button", that also fixed the reloading problem

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+<<<<<<< Updated upstream
+=======
+//FUNCTIONALITY FOR FORM ON HOME PAGE
+
+let signUpBtn = document.querySelector(".signup-btn")
+let thanksforSigning = document.querySelector(".thanks-form")
+let emailName = document.querySelector(".email-name")
+let email = document.querySelector(".email")
+
+signUpBtn.addEventListener("click", function submit() {
+  let thankYou = document.createElement("h2");
+  thankYou.innerText = " Thank you for signing up!";
+  thanksforSigning.append(thankYou);
+  emailName.value = ""
+  email.value = ""
+  setTimeout(function () {
+    thankYou.innerText = " "
+  },5000)
+})
+
+//FUNCTIONALITY FOR PAGE 3
+>>>>>>> Stashed changes
 let submitBtn = document.querySelector(".subBtn");
 let inputSub = document.querySelector(".inputSub");
 let thanks = document.querySelector(".thanks");
@@ -7,7 +29,14 @@ let Thanks = submitBtn.addEventListener("click", function () {
   thankYou.innerText = " The world thanks you for your donation!";
   thanks.append(thankYou);
   inputSub.value = "";
+<<<<<<< Updated upstream
 });
+=======
+  setTimeout(function () {
+    thankYou.innerText = " "
+  },5000)
+})
+>>>>>>> Stashed changes
 
 let thanksTimer = setTimeout(function clear(Thanks) {
   Thanks.clear();
